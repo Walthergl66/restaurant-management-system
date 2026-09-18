8bd58e52e52c1437c97bfdac02a55147e89d114a
walthergl66 <waltherjr68@gmail.com>
2026-09-17 21:32:19 -0500

    feat: crea solucion Restaurant con proyectos por Clean Architecture (BACK-001)

---
+++ backend/tests/Restaurant.ArchitectureTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.ArchitectureTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/tests/Restaurant.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
